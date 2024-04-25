bed2610a5be71c252105c0d47210d4becbd47e36
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2024-04-24 23:11:57 -0400

    Update

--- Scenes/UI/fight_ui.gd
+++ Scenes/UI/fight_ui.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 var labelPlayer = null
 	
 func setLive(value, playerNumber):
-	labelPlayer = get_node('UI/HBoxContainer/Player' + str(playerNumber))
+	var labelPlayer = get_node('UI/HBoxContainer/Player{playerNumber}/PlayerLabel'.format({"playerNumber": playerNumber}))
 	labelPlayer.text = str(value, "%")
 	
-func getLive(value):
+func getLive(value): 
 	return labelPlayer.text
